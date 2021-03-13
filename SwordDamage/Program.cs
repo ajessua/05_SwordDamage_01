06fdff837f2b357aac583431940e13ade4d9681a
Amanda M. Jessup <mandajessup@gmail.com>
2021-03-13 12:36:56 -0500

    Add project files.

---
+++ SwordDamage/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SwordDamage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            SwordDamage swordDamage = new SwordDamage();
+
+
+            while (true)
+            {
+                
+                Console.WriteLine("\n0 for no magic/flaming\n1 for magic\n2 for flaming\n3 for both\n**Hit any other key to quit**");
+                char key = Console.ReadKey().KeyChar;
+                Console.Clear();
+                
+                if (key != '0' && key !='1' && key != '2' && key != '3')
+                {
+                    
+                    return;
+                }
+
+                int roll = rand.Next(1, 7) + rand.Next(1, 7) + rand.Next(1, 7);
+                swordDamage.roll = roll;
+                swordDamage.SetMagic(key == '1' || key == '3');
+                swordDamage.SetFlaming(key == '2' || key == '3');
+                Console.WriteLine("\nRolled " + roll + " for " + swordDamage.damage + " HP\n");
+                Console.ReadLine();
+                Console.Clear();
+
+
+            }
+
+        }
+    }
+}
---
+++ SwordDamage/SwordDamage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwordDamage
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int roll;
+        public decimal magicMultiplier = 1m;
+        public int flamingDamage = 0;
+        public int damage;
+
+
+        public void CalculateDamage()
+        {
+            damage = (int)(roll * magicMultiplier) + BASE_DAMAGE + flamingDamage;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                magicMultiplier = 1.75m;
+            }
+            else
+            {
+                magicMultiplier = 1m;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                damage += FLAME_DAMAGE;
+            }
+        }
+
+    }
+}
